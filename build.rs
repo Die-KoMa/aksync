a50eef0c3c4373143a000e1aa3529ffd6c5658bc
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-05-17 18:26:16 +0200

    chore: initial commit

---
+++ build.rs
@@ -0,0 +1,39 @@
+// © 2025 Maximilian Marx
+// SPDX-FileContributor: Maximilian Marx
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+use clap::{CommandFactory, ValueEnum};
+use clap_complete::{Shell, generate_to};
+use clap_mangen::Man;
+use std::env;
+use std::fs::File;
+use std::io::Error;
+
+include!("src/cli.rs");
+
+fn main() -> Result<(), Error> {
+    let real_outdir = match env::var_os("OUT_DIR") {
+        None => return Ok(()),
+        Some(outdir) => outdir,
+    };
+
+    let outdir = match env::var_os("MAN_OUT") {
+        None => real_outdir,
+        Some(outdir) => outdir,
+    };
+
+    let mut cmd = Cli::command();
+    for &shell in Shell::value_variants() {
+        generate_to(shell, &mut cmd, "aksync", &outdir)?;
+    }
+
+    let file = PathBuf::from(&outdir).join("aksync.1");
+    let mut file = File::create(file)?;
+
+    Man::new(cmd).render(&mut file)?;
+
+    println!("cargo:warning=completion file is generated: {outdir:?}");
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// © 2025 Maximilian Marx
+// SPDX-FileContributor: Maximilian Marx
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+mod cli;
+
+use clap::Parser;
+use cli::Cli;
+
+fn main() {
+    let _args = Cli::parse();
+}
