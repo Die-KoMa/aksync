bb8d905b6d9abe37a9278e8de846f977c19c8195
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-06-14 02:13:50 +0200

    feat(model): add basic model

--- src/model.rs
+++ src/model.rs
@@ -7,74 +7,75 @@ pub mod aktool {
     use serde::{Deserialize, Serialize};
     use std::collections::HashSet;
 
-    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash)]
+    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash, Copy, Clone)]
     #[serde(transparent)]
     pub struct EventId(u64);
 
-    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash)]
+    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash, Copy, Clone)]
     #[serde(transparent)]
     pub struct AKId(u64);
 
-    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash)]
+    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash, Copy, Clone)]
     #[serde(transparent)]
     pub struct CategoryId(u64);
 
-    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash)]
+    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash, Copy, Clone)]
     #[serde(transparent)]
     pub struct OwnerId(u64);
 
-    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash)]
+    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash, Copy, Clone)]
     #[serde(transparent)]
     pub struct TypeId(u64);
 
-    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash)]
+    #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash, Copy, Clone)]
     #[serde(transparent)]
     pub struct RequirementId(u64);
 
     #[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
     pub struct AK {
-        id: AKId,
-        name: String,
-        short_name: String,
-        description: String,
-        link: String,
-        protocol_link: String,
-        reso: bool,
-        present: Option<bool>,
-        notes: String,
-        interest: i64,
-        interest_counter: u64,
-        include_in_export: bool,
-        category: CategoryId,
-        track: Option<u64>,
-        event: EventId,
-        owners: HashSet<OwnerId>,
-        types: HashSet<TypeId>,
-        requirements: HashSet<RequirementId>,
-        conflicts: HashSet<AKId>,
-        prerequisites: HashSet<AKId>,
+        pub(crate) id: AKId,
+        pub(crate) name: String,
+        pub(crate) short_name: String,
+        pub(crate) description: String,
+        pub(crate) link: String,
+        pub(crate) protocol_link: String,
+        pub(crate) reso: bool,
+        pub(crate) present: Option<bool>,
+        pub(crate) notes: String,
+        pub(crate) interest: i64,
+        pub(crate) interest_counter: u64,
+        pub(crate) include_in_export: bool,
+        pub(crate) category: CategoryId,
+        pub(crate) track: Option<u64>,
+        pub(crate) event: EventId,
+        pub(crate) owners: HashSet<OwnerId>,
+        pub(crate) types: HashSet<TypeId>,
+        pub(crate) requirements: HashSet<RequirementId>,
+        pub(crate) conflicts: HashSet<AKId>,
+        pub(crate) prerequisites: HashSet<AKId>,
     }
 
     #[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
     pub struct Category {
-        id: CategoryId,
-        name: String,
-        color: String,
-        description: String,
-        present_by_default: bool,
-        event: EventId,
+        pub(crate) id: CategoryId,
+        pub(crate) name: String,
+        pub(crate) color: String,
+        pub(crate) description: String,
+        pub(crate) present_by_default: bool,
+        pub(crate) event: EventId,
     }
 
     #[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
     pub struct Owner {
-        id: OwnerId,
-        name: String,
-        slug: String,
-        institution: String,
-        link: String,
-        event: EventId,
+        pub(crate) id: OwnerId,
+        pub(crate) name: String,
+        pub(crate) slug: String,
+        pub(crate) institution: String,
+        pub(crate) link: String,
+        pub(crate) event: EventId,
     }
 
+    #[cfg(test)]
     mod test {
         use super::*;
         use test_log::test;
@@ -111,13 +112,111 @@ pub mod aktool {
     }
 }
 
-pub use aktool::AKId;
+use std::collections::{HashMap, HashSet};
+
+pub use aktool::{AKId, CategoryId, EventId, OwnerId};
+use anyhow::{Result, anyhow};
 
 #[derive(Debug)]
-pub struct AK {}
+pub struct Event<'event> {
+    owners: HashMap<OwnerId, Owner>,
+    categories: HashMap<CategoryId, Category>,
+    aks: HashMap<AKId, AK<'event>>,
+}
 
-impl AK {
-    fn from_aktool(ak: aktool::AK) -> Self {
-        todo!()
+impl<'event> Event<'event> {
+    fn new<C, O>(categories: C, owners: O) -> Self
+    where
+        C: IntoIterator<Item = aktool::Category>,
+        O: IntoIterator<Item = aktool::Owner>,
+    {
+        Self {
+            categories: HashMap::from_iter(
+                categories
+                    .into_iter()
+                    .map(|category| (category.id, Category::from(category))),
+            ),
+            owners: HashMap::from_iter(
+                owners
+                    .into_iter()
+                    .map(|owner| (owner.id, Owner::from(owner))),
+            ),
+            aks: HashMap::new(),
+        }
+    }
+
+    fn add_ak(&'event mut self, id: AKId, ak: aktool::AK) -> Result<()> {
+        let category = self
+            .categories
+            .get(&ak.category)
+            .ok_or_else(|| anyhow!("unknown category {:?}", ak.category))?;
+        let owners = ak
+            .owners
+            .iter()
+            .map(|owner_id| {
+                self.owners
+                    .get(owner_id)
+                    .ok_or_else(|| anyhow!("unknown owner {:?}", owner_id))
+            })
+            .collect::<Result<_>>()?;
+
+        let ak = AK::from_aktool(ak, category, owners);
+        let _ = self.aks.insert(id, ak);
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct Category {
+    name: String,
+    description: String,
+}
+
+impl From<aktool::Category> for Category {
+    fn from(value: aktool::Category) -> Self {
+        Self {
+            name: value.name,
+            description: value.description,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Owner {
+    name: String,
+    institution: String,
+    link: String,
+}
+
+impl From<aktool::Owner> for Owner {
+    fn from(value: aktool::Owner) -> Self {
+        Self {
+            name: value.name,
+            institution: value.institution,
+            link: value.link,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AK<'event> {
+    name: String,
+    description: String,
+    owners: HashSet<&'event Owner>,
+    category: &'event Category,
+}
+
+impl<'event> AK<'event> {
+    fn from_aktool(
+        ak: aktool::AK,
+        category: &'event Category,
+        owners: HashSet<&'event Owner>,
+    ) -> Self {
+        Self {
+            name: ak.name,
+            description: ak.description,
+            category,
+            owners,
+        }
     }
 }
